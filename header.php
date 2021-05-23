8506f466186b7c48f3dba7c7299ed8c1fa9d270b
Artur <artur133715@gmail.com>
2021-05-23 22:16:46 +0600

    modif: стили, скрипты, шапка

--- header.php
+++ header.php
@@ -1,1 +1,65 @@
-<?php
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lukler</title>
+    <link rel="stylesheet" href="assets/styles/index.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script defer src="/assets/js/main.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script defer src="/assets/js/slider-initialization.js"></script>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header">
+                <div class="header__logo">
+                    <img class="d-none d-lg-block" src="assets/img/logo.png" alt="">
+                    <img class="d-block d-lg-none" src="assets/img/logo-tablet.png" alt="">
+                </div>
+                <div class="header__right">
+                    <div class="header__menu d-none d-lg-block">
+                        <ul class="header__menu-nav">
+                            <li class="nav__item has-child">
+                                <a href="">Меню</a>
+                                <div class="nav__item-children">
+                                    <ul>
+                                        <li>Что-то</li>
+                                        <li>Что-то</li>
+                                        <li>Что-то</li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="nav__item">
+                                <a href="">Шеф-кондитер</a>
+                            </li>
+                            <li class="nav__item has-child">
+                                <a href="">О кафе</a>
+                                <div class="nav__item-children">
+                                    <ul>
+                                        <li>Что-то</li>
+                                        <li>Что-то</li>
+                                        <li>Что-то</li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="nav__item">
+                                <a href="">Контакты</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="header__cart">
+                        <img src="assets/img/header-iamges/header-cart.png" alt="">
+                    </div>
+                    <div class="header__mobile-menu d-block d-lg-none">
+                        <img src="assets/img/header-iamges/mobile-menu.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>

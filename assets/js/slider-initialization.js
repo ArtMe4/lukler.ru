b77a3329ce99cd280393d026e5ebcade84d0dfa6
Artur <artur133715@gmail.com>
2021-05-23 22:18:21 +0600

    modif: инициализация слайдеров

---
+++ assets/js/slider-initialization.js
@@ -0,0 +1,10 @@
+const catalog_swiper = new Swiper('.catalog__slider-slider', {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.catalog__slider-next',
+        prevEl: '.catalog__slider-prev',
+    },
+
+});

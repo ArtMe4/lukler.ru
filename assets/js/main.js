e0bdb34595dbb026255d4698d9af1f0a6cb7cb92
Artur <artur133715@gmail.com>
2021-05-24 23:23:41 +0600

    modif: переинициализация слайдеров при клике на таб

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,10 +12,18 @@ $(function() {
             .removeClass("active")
             .eq($(this).index())
             .addClass("active");
+        catalog_swiper = new Swiper('.catalog__slider-slider .swiper-container', {
+            observer: true,
+            observeParents: true,
+            slidesPerView: "auto",
+            spaceBetween: 10,
+            // Navigation arrows
+            navigation: {
+                nextEl: '.catalog__slider-next',
+                prevEl: '.catalog__slider-prev',
+            }
+        });
     });
-});
-
-$(function() {
     $(".new__tabs ul").on("click", "li:not(.active)", function() {
         $(this)
             .addClass("active")
@@ -26,5 +34,20 @@ $(function() {
             .removeClass("active")
             .eq($(this).index())
             .addClass("active");
+        new_swiper = new Swiper(".new__tabs-content.active .new__tabs-content-slider-bg .swiper-container", {
+            observer: true,
+            observeParents: true,
+            slidesPerView: 1,
+            pagination: {
+                el: ".new__tabs-content.active .new__slider-pagination",
+                type: "fraction",
+            },
+            // Navigation arrows
+            navigation: {
+                nextEl: '.new__tabs-content.active .new__slider-next',
+                prevEl: '.new__tabs-content.active .new__slider-prev',
+            }
+        });
     });
 });
+

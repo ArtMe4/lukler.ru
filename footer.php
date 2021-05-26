037db6a53f22c517436b104c29001d6889987b8d
Artur <artur133715@gmail.com>
2021-05-27 00:24:56 +0600

    modif: закрытие документа и скрипты

--- footer.php
+++ footer.php
@@ -1,1 +1,6 @@
-<?php
+
+        <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+        <script defer src="/assets/js/slider-initialization.js"></script>
+        <script defer src="/assets/js/main.js"></script>
+    </body>
+</html>

6ddb1ba2632bcc9b5c0b011c149b52736af9dc0f
Artur <artur133715@gmail.com>
2021-06-01 00:07:42 +0600

    modif: функционал рейтинга и изменения цены в соответствии с количествои в карточке

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,3 +51,37 @@ $(function() {
     });
 });
 
+// Изменение цены и количества в карточке товара
+
+$(document).ready(function() {
+    function change($tr, val) {
+        var $input = $tr.find('.buttons__count-quantity');
+        var count = parseInt($input.val()) + val;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        var $price = $tr.parent().parent().parent().parent().find('.price__new');
+        var $price_old = $tr.parent().parent().parent().parent().find('.price__old');
+        $price.text(count * $price.data('price') + ' ₽');
+        $price_old.text(count * $price_old.data('price') + ' ₽');
+    }
+    $('.buttons__count-minus').click(function() {
+        change($(this).closest('.buttons__count'), -1);
+    });
+    $('.buttons__count-plus').click(function() {
+        change($(this).closest('.buttons__count'), 1);
+    });
+    $('.buttons__count-quantity').on("input", function() {
+        var $price = $(this).parent().parent().parent().parent().find('.price__new');
+        var $price_old = $(this).parent().parent().parent().parent().find('.price__old');
+        $price.text(this.value * $price.data('price') + ' ₽');
+        $price_old.text(this.value * $price_old.data('price') + ' ₽');
+    });
+});
+
+// Рейтинг в звездах
+
+$(document).ready(function() {
+    var total_rating = $('.product__info-star-mask').data('star');
+    var star_width = total_rating * 20 + Math.floor(total_rating) * 5;
+    $('.product__info-star-mask').width(star_width);
+});
